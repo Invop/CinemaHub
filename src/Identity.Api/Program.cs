50a1022faccb2d2a341ffbc5b3a9cadb8b2d94ad
Andrii Shandryk <shandrykjj512@gmail.com>
2024-09-28 15:59:34 +0300

    Add database migration support and Identity configuration
    Added migration tracing and database seeder extension methods. Integrated IdentityServer configurations and updated solution/project files to include new services for enhanced database and identity management.

--- src/Identity.Api/Program.cs
+++ src/Identity.Api/Program.cs
@@ -1,34 +1,46 @@
-using Identity.Api.Data;
-using Identity.Api.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
+using Identity.Api.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+builder.Services.AddControllersWithViews();
+builder.AddNpgsqlDbContext<ApplicationDbContext>("identitydb");
 
-// Configure logging
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
-
-// Ensure configuration is loaded
-var configuration = builder.Configuration;
-
-// Setup connection string diagnostics
-var connectionString = configuration.GetConnectionString("DefaultConnection");
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString));
-// Add Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
+builder.Services.AddIdentityServer(options =>
+    {
+        //options.IssuerUri = "null";
+        options.Authentication.CookieLifetime = TimeSpan.FromHours(2);
+
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+
+        // TODO: Remove this line in production.
+        options.KeyManagement.Enabled = false;
+    })
+    .AddInMemoryIdentityResources(Config.GetResources())
+    .AddInMemoryApiScopes(Config.GetApiScopes())
+    .AddInMemoryApiResources(Config.GetApis())
+    .AddInMemoryClients(Config.GetClients(builder.Configuration))
+    .AddAspNetIdentity<ApplicationUser>()
+// TODO: Not recommended for production - you need to store your key material somewhere secure
+    .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
+app.MapDefaultEndpoints();
+
+app.UseStaticFiles();
+
+// This cookie policy fixes login issues with Chrome 80+ using HTTP
+app.UseCookiePolicy(new CookiePolicyOptions { MinimumSameSitePolicy = SameSiteMode.Lax });
+app.UseRouting();
+app.UseIdentityServer();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapDefaultControllerRoute();
 
 app.Run();
