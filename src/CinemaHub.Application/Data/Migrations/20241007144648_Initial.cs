6da38e5ae9695915f8e797d1c6e54695bf270453
Andrii Shandryk <shandrykjj512@gmail.com>
2024-10-07 17:51:31 +0300

    Add initial database migration
    This commit introduces the initial database migration, creating tables for movies, genres, and ratings. It defines the relationships and constraints between these tables for the MovieDbContext. The generated code includes schema definitions and migration logic in accordance with Entity Framework Core conventions.

---
+++ src/CinemaHub.Application/Data/Migrations/20241007144648_Initial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaHub.Application.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Rating = table.Column<float>(type: "real", nullable: true),
+                    UserRating = table.Column<int>(type: "integer", nullable: true),
+                    YearOfRelease = table.Column<int>(type: "integer", nullable: false),
+                    Slug = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_movies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "genres",
+                columns: table => new
+                {
+                    MovieId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_genres", x => new { x.MovieId, x.Name });
+                    table.ForeignKey(
+                        name: "FK_genres_movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ratings",
+                columns: table => new
+                {
+                    MovieId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Slug = table.Column<string>(type: "text", nullable: false),
+                    Rating = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ratings", x => new { x.UserId, x.MovieId });
+                    table.ForeignKey(
+                        name: "FK_ratings_movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_movies_Slug",
+                table: "movies",
+                column: "Slug",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ratings_MovieId",
+                table: "ratings",
+                column: "MovieId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "genres");
+
+            migrationBuilder.DropTable(
+                name: "ratings");
+
+            migrationBuilder.DropTable(
+                name: "movies");
+        }
+    }
+}
