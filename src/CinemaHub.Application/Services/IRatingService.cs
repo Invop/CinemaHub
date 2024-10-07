1136be1bb1bee2038e24a785b0c717869b8ce35c
Andrii Shandryk <shandrykjj512@gmail.com>
2024-10-07 18:10:55 +0300

    Add interfaces for rating and movie services and repositories
    Create IRatingService and IRatingRepository interfaces to handle movie ratings. Introduce IMovieService and IMovieRepository interfaces for managing movies. These changes modularize the application, promoting code reusability and separation of concerns.

---
+++ src/CinemaHub.Application/Services/IRatingService.cs
@@ -0,0 +1,12 @@
+﻿using CinemaHub.Application.Models;
+
+namespace CinemaHub.Application.Services;
+
+public interface IRatingService
+{
+    Task<bool> RateMovieAsync(Guid movieId, int rating, Guid userId, CancellationToken token = default);
+
+    Task<bool> DeleteRatingAsync(Guid movieId, Guid userId, CancellationToken token = default);
+
+    Task<IEnumerable<MovieRating>> GetRatingsForUserAsync(Guid userId, CancellationToken token = default);
+}
