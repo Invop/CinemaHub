1b631619f952ab6d8660d04ee260ea0eef38a6b4
Andrii Shandryk <shandrykjj512@gmail.com>
2024-10-07 20:06:18 +0300

    Remove initial migration and related database setup
    Deleted Initial migration and configuration files from the project, including MoviesDbContextModelSnapshot. Adjusted DbContext to exclude entity configurations as they are no longer needed. Updated project file to maintain folder structure for potential future migrations.

--- src/CinemaHub.Application/Database/CinemaHubDbContext.cs
+++ src/CinemaHub.Application/Database/CinemaHubDbContext.cs
@@ -16,50 +16,6 @@ public class MoviesDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Movie>(entity =>
-        {
-            entity.ToTable("movies");
 
-            entity.HasKey(e => e.Id);
-
-            entity.HasIndex(e => e.Slug).IsUnique();
-
-            entity.Property(e => e.Slug).IsRequired();
-
-            entity.Property(e => e.Title).IsRequired();
-
-            entity.Property(e => e.YearOfRelease).IsRequired();
-
-            entity.Ignore(e => e.Genres);  // Ignored because genres are mapped separately
-        });
-
-        modelBuilder.Entity<MovieRating>(entity =>
-        {
-            entity.ToTable("ratings");
-
-            entity.HasKey(e => new { e.UserId, e.MovieId });
-
-            entity.Property(e => e.Rating).IsRequired();
-
-            entity.HasOne<Movie>()
-                .WithMany()
-                .HasForeignKey(e => e.MovieId);
-        });
-
-        modelBuilder.Entity<Genre>(entity =>
-        {
-            entity.ToTable("genres");
-
-            entity.HasKey(e => new { e.MovieId, e.Name });
-
-            entity.Property(e => e.Name).IsRequired();
-
-            entity.HasOne(e => e.Movie)
-                .WithMany(m => m.Genres)
-                .HasForeignKey(e => e.MovieId)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        base.OnModelCreating(modelBuilder);
     }
 }
