8f520e76acac9f51a389e3a5d8ccee5f8391d0a0
Andrii Shandryk <shandrykjj512@gmail.com>
2024-10-08 21:54:26 +0300

    Refactor MoviesController and implement output caching
    Refactored the MoviesController to streamline user ID retrieval with a new private method. Implemented output caching for Get and GetAll endpoints, and evicted cache upon movie creation and updates for better performance. Updated Program.cs to configure and apply cache policies.

--- src/MovieHub.Api/Program.cs
+++ src/MovieHub.Api/Program.cs
@@ -1,24 +1,40 @@
-using MovieHub.Api;
 using MovieHub.Api.Mapping;
 using MovieHub.Application;
 using MovieHub.Application.Data;
 using MovieHub.ServiceDefaults;
 
 var builder = WebApplication.CreateBuilder(args);
-var config = builder.Configuration;
+
 builder.AddServiceDefaults();
 builder.AddApplicationServices();
 
 var withApiVersioning = builder.Services.AddApiVersioning();
-
 builder.AddDefaultOpenApi(withApiVersioning);
+
 builder.AddNpgsqlDbContext<MovieDbContext>("moviesdb");
 builder.Services.AddDatabase();
-builder.Services.AddControllers();
+
+
 builder.Services.AddApplication();
 
+builder.Services.AddOutputCache(x =>
+{
+    x.AddBasePolicy(c => c.Cache());
+    x.AddPolicy("MovieCache", c => 
+        c.Cache()
+            .Expire(TimeSpan.FromMinutes(1))
+            .SetVaryByQuery(["title", "year", "sortBy", "page", "pageSize"])
+            .Tag("movies"));
+});
+
+builder.Services.AddControllers();
+
+
 var app = builder.Build();
+
 app.MapDefaultEndpoints();
+app.UseOutputCache();
+
 app.UseMiddleware<ValidationMappingMiddleware>();
 app.MapControllers();
 
